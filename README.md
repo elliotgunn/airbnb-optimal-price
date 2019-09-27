e853dfee28fcf492b4f3dc3bf2ff107ac7d28c8c
91a3b0 <elliot.j.gunn@gmail.com>
2019-09-27 15:29:29 -0400

    deployed

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ This is the data engineering part of the data science MVP. It consists of creati
 
 The data science part consists of data cleaning, wrangling, and trained on a model. The model was pickled for deployment.
 
-The app works by processing incoming data in Flask using the `request` object.
+The app works by processing incoming data in Flask using the `request` object:
+
+```{"feature":[{"id":3,"neighbourhood_group_cleansed":"maybe","description":"its is a description","property_type":"huge","accommodates":2,"bathrooms":1,"security_deposit":200,"cleaning_fee":3,"guests_included":0,"extra_people":0,"minimum_nights":1,"instant_bookable":false,"cancellation_policy":"big money","tv_cable":false,"pets_allowed":false,"bedrooms":1},{"id":2,"neighbourhood_group_cleansed":"maybe","description":"its is a description","property_type":"huge","accommodates":0,"bathrooms":1,"security_deposit":200,"cleaning_fee":3,"guests_included":0,"extra_people":0,"minimum_nights":1,"instant_bookable":false,"cancellation_policy":"big money","tv_cable":true,"pets_allowed":false,"bedrooms":7}]}```
 
 receives JSON data, converts it to a numpy array, generates a prediction, and then (sends to FE?) as JSON data again.
 
@@ -24,3 +26,6 @@ bedrooms:
 Helpful resources:
 
 [How to Process Incoming Request Data in Flask](https://www.youtube.com/watch?v=hAEJajltHxc&amp=&index=129)
+
+To test and verify, run 'FLASK_APP=flask-api.py flask shell`
+
--- flask-api.py
+++ flask-api.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 import numpy as np
+import pandas as pd
 import pickle
+import requests
 from gensim.utils import simple_preprocess
 from gensim.parsing.preprocessing import STOPWORDS
 
@@ -13,49 +15,73 @@ encoder = pickle.load(open('encoder.pkl', 'rb'))
 
 # Function used for processing description
 STOPWORDS = set(STOPWORDS).union(set(['and']))
+
+
 def tokenize(text):
-    return [token for token in simple_preprocess(text) if token not in STOPWORDS]
+    return [
+        token for token in simple_preprocess(text) if token not in STOPWORDS
+        ]
+
+# Connect to backend
+@app.route('/', methods=['POST', 'GET'])
+def responses():
+    response = requests.get(
+        'https://bnb-web-backend.herokuapp.com/api/features')
+    return str(response.text)
 
 # the route() binds a function to a URL
 @app.route('/api', methods=['POST', 'GET'])
-def api():
+def predict():
     """
     Receives input data as JSON and returns optimal price of AirBnB unit.
-
     """
+    # retrieve json user input data
+    data = request.get_json(force=True)
 
-    # 1. access data
-    # force = True: Ignore the mimetype and always try to parse JSON
-    req_data = request.get_json(force=True)
+    # assign incoming data to variables for use in model
+    desc = data['feature'][0]['description']
 
-    # Assign incoming data to variables for use in model
-    desc = req_data['description']
-
-    # Run description through tokenize function
+    # run description through tokenize function
     description_length = len(tokenize(desc))
 
+    # continue to assign incoming data to variables for use in model
+    neighbourhood_group_cleansed = (
+        data['feature'][0]['neighbourhood_group_cleansed'])
+    property_type = data['feature'][0]['property_type']
+    accommodates = data['feature'][0]['accommodates']
+    bathrooms = data['feature'][0]['bathrooms']
+    bedrooms = data['feature'][0]['bedrooms']
+    security_deposit = data['feature'][0]['security_deposit']
+    cleaning_fee = data['feature'][0]['cleaning_fee']
+    guests_included = data['feature'][0]['guests_included']
+    extra_people = data['feature'][0]['extra_people']
+    minimum_nights = data['feature'][0]['minimum_nights']
+    instant_bookable = data['feature'][0]['instant_bookable']
+    cancellation_policy = data['feature'][0]['cancellation_policy']
+    tv_cable = data['feature'][0]['tv_cable']
+    pets_allowed = data['feature'][0]['pets_allowed']
 
-    # TO DO: fill in inputs
-    neighbourhood_group_cleansed = req_data['neighbourhood_group_cleansed']
-    property_type = req_data['property_type']
-    accommodates = req_data['accommodates']
-    bathrooms = req_data['bathrooms']
-    bedrooms = req_data['bedrooms']
-    security_deposit = req_data['security_deposit']
-    cleaning_fee = req_data['cleaning_fee']
-    guests_included = req_data['guests_included']
-    extra_people = req_data['extra_people']
-    minimum_nights = req_data['minimum_nights']
-    instant_bookable = req_data['instant_bookable']
-    cancellation_policy = req_data['cancellation_policy']
-    tv_cable = req_data['tv_cable']
-    pets_allowed = req_data['pets_allowed']
+    # create dict for model
+    feature_dict = {
+        'neighbourhood_group_cleansed': neighbourhood_group_cleansed,
+        'property_type': property_type,
+        'accommodates': accommodates,
+        'bathrooms': bathrooms,
+        'bedrooms': bedrooms,
+        'security_deposit': security_deposit,
+        'cleaning_fee': cleaning_fee,
+        'guests_included': guests_included,
+        'extra_people': extra_people,
+        'minimum_nights': minimum_nights,
+        'instant_bookable': instant_bookable,
+        'cancellation_policy': cancellation_policy,
+        'description_length': description_length,
+        'tv': tv_cable,
+        'pets': pets_allowed
+        }
 
-    # 2. convert to list for model
-    features = [description, neighbourhood_group_cleansed, property_type, accommodates,
-                bathrooms, bedrooms, security_deposit, cleaning_fee, guests_included,
-                extra_people, minimum_nights, instant_bookable, cancellation_policy,
-                tv_cable, pets_allowed]
+    # create dataframe
+    features = pd.DataFrame(feature_dict, index=[1])
 
     # run the features through the encoder
     features_transformed = encoder.transform(features)
@@ -63,11 +89,9 @@ def api():
     # predict optimal price using the prediction function
     price = model.predict(features_transformed)
 
+    # return prediction in json format
     return jsonify({'prediction': price})
 
 if __name__ == '__main__':
-    # can also load pickled file here
 
-    # e.g. modelfile = 'models/final_prediction.pickle'
-    # model = p.load(open(modelfile, 'rb'))
-    app.run(debug=True, port=5000)
+    app.run(debug = True)
